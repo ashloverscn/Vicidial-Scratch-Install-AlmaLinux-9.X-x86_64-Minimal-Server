a7b095a24870e36fd9863079a7a61447c1d12752
ashloverscn <ashloverscn@gmail.com>
2026-09-13 12:21:03 +0530

    Update part9.install-vicidial-src-from-svn-repo.sh

--- part9.install-vicidial-src-from-svn-repo.sh
+++ part9.install-vicidial-src-from-svn-repo.sh
@@ -37,7 +37,7 @@ mysql -uroot -e "flush privileges;"
 mysql -uroot -e "SET GLOBAL connect_timeout=60;"
 mysql -uroot asterisk < /usr/src/astguiclient/trunk/extras/MySQL_AST_CREATE_tables.sql
 mysql -uroot asterisk < /usr/src/astguiclient/trunk/extras/first_server_install.sql
-mysql -uroot -e "use asterisk ; update servers set asterisk_version='18.21.0-vici';"
+mysql -uroot -e "use asterisk ; update servers set asterisk_version='18.26.4';"
 mysql -uroot -e "use asterisk ; show tables;"
 sleep 5
 
